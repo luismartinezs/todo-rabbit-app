02d4985386b83ebad0d8200d57f1bb75b41251bf
Luis Martinez <luis@passionatepeople.io>
2022-05-20 10:31:42 +0200

    style: login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,38 +19,39 @@ const Login = () => {
     event.preventDefault();
     createAccount(email, password);
   };
+
   return (
-    <>
-      <form className="flex flex-col space-y-4">
-        <h2 className="font-bold">Login</h2>
-        <input
-          type="text"
-          onChange={handleEmailChange}
-          placeholder="email"
-          value={email}
-        />
-        <input
-          type="text"
-          onChange={handlePasswordChange}
-          placeholder="password"
-          value={password}
-        />
-        <button
-          className="border border-primary-500 hover:bg-primary-200"
-          type="button"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
-        <button
-          className="border border-primary-500 hover:bg-primary-200"
-          type="button"
-          onClick={handleRegister}
-        >
-          Register
-        </button>
-      </form>
-    </>
+    <div className="flex flex-col space-y-4">
+      <h2 className="font-bold text-2xl">Login</h2>
+      <input
+        type="text"
+        onChange={handleEmailChange}
+        placeholder="email"
+        value={email}
+        className="py-2 px-3 rounded border border-primary-300"
+      />
+      <input
+        type="text"
+        onChange={handlePasswordChange}
+        placeholder="password"
+        value={password}
+        className="py-2 px-3 rounded border border-primary-300"
+      />
+      <button
+        className="py-2 px-3 bg-primary-500 rounded text-white text-xl font-bold hover:bg-primary-600"
+        type="button"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+      <button
+        className="py-2 px-3 bg-primary-500 rounded text-white text-xl font-bold hover:bg-primary-600"
+        type="button"
+        onClick={handleRegister}
+      >
+        Register
+      </button>
+    </div>
   );
 };
 
