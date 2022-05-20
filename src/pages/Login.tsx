706428a039e26e54816401a69de84a24c8caa9d9
Luis Martinez <luis@passionatepeople.io>
2022-05-20 11:19:40 +0200

    feat: add reset and google buttons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
-import { loginEmailPassword, createAccount } from "@/firebase/auth";
+import { useState, useEffect } from "react";
+import {
+  auth,
+  loginEmailPassword,
+  createAccount,
+  signInWithGoogle,
+} from "@/firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("saito200@gmail.com");
   const [password, setPassword] = useState("admin1234");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    // if (user) navigate("/");
+  }, [user, loading]);
+
   const handleLogin = (event) => {
-    event.preventDefault();
     loginEmailPassword(email, password);
   };
   const handleRegister = (event) => {
@@ -25,32 +35,42 @@ const Login = () => {
       <h2 className="font-bold text-2xl">Login</h2>
       <input
         type="text"
-        onChange={handleEmailChange}
+        onChange={(e) => setEmail(e.target.value)}
         placeholder="email"
         value={email}
         className="py-2 px-3 rounded border border-primary-300"
       />
       <input
         type="text"
-        onChange={handlePasswordChange}
+        onChange={(e) => setPassword(e.target.value)}
         placeholder="password"
         value={password}
         className="py-2 px-3 rounded border border-primary-300"
       />
       <button
-        className="py-2 px-3 bg-primary-500 rounded text-white text-xl font-bold hover:bg-primary-600"
+        className="py-2 px-3 bg-gray-800 rounded text-white text-xl font-bold hover:bg-black"
         type="button"
-        onClick={handleLogin}
+        onClick={() => loginEmailPassword(email, password)}
       >
         Login
       </button>
       <button
         className="py-2 px-3 bg-primary-500 rounded text-white text-xl font-bold hover:bg-primary-600"
         type="button"
-        onClick={handleRegister}
+        onClick={signInWithGoogle}
+      >
+        Login with Google
+      </button>
+      <button
+        className="py-2 px-3 bg-primary-500 rounded text-white text-xl font-bold hover:bg-primary-600"
+        type="button"
+        onClick={() => createAccount(email, password)}
       >
         Register
       </button>
+      <div>
+        <Link to="/reset">Forgot Password</Link>
+      </div>
     </div>
   );
 };
