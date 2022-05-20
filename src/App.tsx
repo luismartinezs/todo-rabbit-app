f7793131a0757a48c3398b8e77bcf7802122cf29
Luis Martinez <luis@passionatepeople.io>
2022-05-20 11:04:45 +0200

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Routes, Route, Link } from "react-router-dom";
 import Todos from "@/pages/Todos";
 import Login from "@/pages/Login";
 
 function App(): JSX.Element {
-
   return (
     <div className="App flex flex-col justify-center items-center w-full py-10 space-y-6  items-center">
-      {/* <Todos /> */}
-      <Login />
+      <Routes>
+        <Route path="/" element={<Todos />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
