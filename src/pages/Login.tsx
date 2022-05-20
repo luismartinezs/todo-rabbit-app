cbed459b6b3e707790cbe323da0eb4407228993f
Luis Martinez <luis@passionatepeople.io>
2022-05-20 08:32:08 +0200

    feat: static login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,61 @@
-const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+import { useState } from "react";
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleLoginPasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleRegisterEmailChange = (event) => {
+    console.log(event.target.value);
+  };
+  const handleRegisterPasswordChange = (event) => {
+    console.log(event.target.value);
+  };
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <>
+      <form className="flex flex-col space-y-4" onSubmit={handleLogin}>
+        <h2 className="font-bold">Login</h2>
+        <input
+          type="text"
+          onChange={handleLoginEmailChange}
+          placeholder="email"
+        />
+        <input
+          type="text"
+          onChange={handleLoginPasswordChange}
+          placeholder="password"
+        />
+        <button
+          className="border border-primary-500 hover:bg-primary-200"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+      <form className="flex flex-col space-y-4">
+        <h2>Register</h2>
+        <input
+          type="text"
+          onChange={handleRegisterEmailChange}
+          placeholder="email"
+        />
+        <input
+          type="text"
+          onChange={handleRegisterPasswordChange}
+          placeholder="password"
+        />
+      </form>
+    </>
+  );
+};
+
+export default Login;
