f6fced0fa04435c5b7081945e12673bf5930fb3c
Luis Martinez <luis@passionatepeople.io>
2022-05-22 20:23:43 +0200

    style: button hover

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -48,7 +48,7 @@ const Todos = () => {
       <div className="">
         <div className="flex items-center space-x-2">
           <div>Logged in as {user?.email}</div>
-          <button className="border border-primary-500 rounded py-1 px-2" onClick={logout}>
+          <button className="border border-primary-500 rounded py-1 px-2 hover:bg-primary-500 hover:text-white" onClick={logout}>
             Logout
           </button>
         </div>
@@ -64,7 +64,7 @@ const Todos = () => {
           placeholder="Make Todo"
         />
         <button
-          className="uppercase bg-primary-500 text-white py-2 px-8 rounded font-semibold"
+          className="uppercase bg-primary-500 text-white py-2 px-8 rounded font-semibold hover:bg-primary-600"
           onClick={addTodo}
         >
           Add Todo
